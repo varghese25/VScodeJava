b483fdc402875bcc8953be66cedf4af6ac43558e
varghese25 <vbpoulose@gmail.com>
2024-07-22 17:44:11 -0400

    ProSwitchCase

---
+++ Pro.java
@@ -0,0 +1,22 @@
+public class Pro {
+    public static void main(String[] args) {
+        int day = 5;
+        switch (day) { // Expression
+            case 1:
+                System.out.println("Monday"); // Code block
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Weneday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday"); // out Put will Friday
+                break;
+        }
+    }
+}
