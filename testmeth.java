40c943e0c46dfdb4aa81cf03dacea6d1710d6cac
varghese25 <vbpoulose@gmail.com>
2024-08-09 18:55:45 -0400

    Todays Pratice

---
+++ testmeth.java
@@ -0,0 +1,24 @@
+public class testmeth {
+    // static void myMethod() { // method
+    // System.out.println("Just testing myMethod");
+    // }
+
+    // public static void main(String[] args) {
+
+    // myMethod(); // mymethod main class
+    // myMethod(); // multiple method
+    // myMethod(); // multiple method
+    // }
+    // }
+    static void newMethod(String fname) {
+        System.out.println(fname + " Tiju");
+
+    }
+
+    public static void main(String[] args) {
+
+        newMethod("Varghese");
+        newMethod("Shannon");
+        newMethod("Evan");
+    }
+}
