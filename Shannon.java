f2b91f9dd977c8fb60823195db9bde14fb53dc10
varghese25 <vbpoulose@gmail.com>
2024-09-06 14:33:40 -0400

    06-09-2024 Updated Code

--- Shannon.java
+++ Shannon.java
@@ -1,14 +1,13 @@
 public class Shannon {
-private String name;
+    private String name;
 
+    // Getter
+    public String getName() { // From Shannon 2 class Getter is used read only
+        return name;
+    }
+    // setter
 
-// Getter
-public String getName() { // From Shannon 2 class Getter is used read only
-    return name;
-}
-//setter
-
-public void setName(String newName){ // Shannon 2 class Setter we give name write only
-this.name =newName;
-} 
+    public void setName(String newName) { // Shannon 2 class Setter we give name write only
+        this.name = newName;
+    }
 }
